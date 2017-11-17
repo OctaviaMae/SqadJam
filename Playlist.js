7aaf634362584109fb4b6e4700d75588c6c30407
OctaviaMae <octavia.smith40@gmail.com>
2017-11-16 19:26:34 -0500

    Update Playlist.js

--- Playlist.js
+++ Playlist.js
@@ -2,7 +2,7 @@
 in Playlist.xml. This file is also responsible for the UI features suck as button typing,
 swiping and populating our lists from the http request.
 
-@Author Octavia Smith, Raphael Wies, Eric Hartig with help from Preston Wang-Stausser-Basset
+@Author Octavia Smith, Raphael Wies, Azra Arab and Eric Hartig with help from Preston Wang-Stausser-Basset
 @Date November 3, 2017
 
 
@@ -12,10 +12,45 @@ swiping and populating our lists from the http request.
 var gestures = require('ui/gestures');
 var frameModule = require('ui/frame');
 var listViewModule = require('ui/list-view');
+var labelModules = require('ui/label');
+var http = require("http");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var observableModule = require("data/observable").Observable;
+var fetchModule = require("fetch");
 
 
 
-var navigateLogin = {
+/*
+The puropse of this function is to gather information 
+*/
+exports.loaded = function(args){
+
+var page = args.object;
+
+var url = "http://46cdb31b.ngrok.io/api/search/" + "beyonce"; //beyonce is a test value
+
+console.log(url);
+
+fetchModule.fetch(url).then(response => {return response.json();}).then(function(r){
+      console.log(JSON.stringify(r.entries));
+       page.bindingContext = r;
+       console.log("DONE");
+     }
+     );
+}
+function createViewModel() {
+    var viewModel = new Observable();
+    viewModel.counter = 0;
+    viewModel.message = countLikes(viewModel.counter);
+
+    viewModel.onTap = function() {
+        this.counter++;
+        this.set("Like", countLikes(this.counter));
+    }
+
+    return viewModel;
+}
+/*var navigateLogin = {
   moduleName: 'views/login/login',
   animated: true,
   backstackVisible: false,
@@ -29,12 +64,7 @@ var navigateLogin = {
  function onNavigatingTo(args) {
    var page = args.object;
 
-   // Define swipable gestures and their actions
-   /*var myStack = page.getViewById('swipable');
-   myStack.on(gestures.GestureTypes.swipe, function(args) {
-     if(args.direction == gestures.SwipeDirection.right) {
-       // When swiping right, navigate home using the behavior defined in the global navigateHome object
-       frameModule.topmost().navigate(navigateLogin);
+ 
      }
    });*/
 
